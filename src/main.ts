3dc0cd176be1188bd04373fff363f876d0688f60
rodricsls <srodri72003@gmail.com>
2024-12-13 19:28:04 -0600

    otra vez

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { bootstrapCameraKit } from "@snap/camera-kit";
 (async function () {
   const cameraKit = await bootstrapCameraKit({
     apiToken:
-      "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM0MDU5Nzc3LCJzdWIiOiJlNjIzMWU2Ni1lYjhhLTQxNGUtYjBlYy01MDFjNmE4ZDVjZTh-UFJPRFVDVElPTn4xOTNmNDgzMy1hZmJkLTQ4NjgtOTZmMy1hMzYzOWQxNTM4ZmUifQ.8rLuTct31Abt9q7bs8NvaTEe4BLkSefElzF2BZtM7Gc",
+      "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM0MDU5Nzc3LCJzdWIiOiJlNjIzMWU2Ni1lYjhhLTQxNGUtYjBlYy01MDFjNmE4ZDVjZTh-U1RBR0lOR340YmQwM2NmMi01NGVlLTQ1YmQtODUyYi05YjcxYTk1MDM2NjIifQ.GNZfZUIBF-wh_5YByeVKK0hcED0UsBxZlapv6W8gi_A",
   });
 
   const liveRenderTarget = document.getElementById(
